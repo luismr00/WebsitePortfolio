45702e013c95fe6b85f04ad54cd2b2acfea2742a
Luis Rangel <luismrangel7@gmail.com>
2021-10-05 20:27:19 -0700

    added new projects to sites

--- src/project-components/Sites.jsx
+++ src/project-components/Sites.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import YoutubeEmbed from "./YoutubeEmbed.jsx";
 
 class SiteRender extends React.Component {
   constructor(props) {
@@ -16,7 +17,15 @@ class SiteRender extends React.Component {
       <section className="site-container">
         <div className="center-site">
           <div className="site-description">
-            <p>Page is currently under maintenance</p>
+
+            <h1>Snap Nourish</h1>
+            <YoutubeEmbed embedId="i0sDY-NdTZY" />
+            <a href="https://github.com/ssotoale/SnapNourish" title="snap nourish" target="_blank">Check out the GitHub Repo for Snap Nourish</a>
+            
+            <h1>Comment Hunter</h1>
+            <YoutubeEmbed embedId="6uP6VD8ME-E"/>
+            <a href="https://github.com/comment-hunter/comment-hunter" title="comment hunter" target="_blank">Check out the GitHub Repo for Comment Hunter</a>
+
           </div>
         </div>
       </section>
